678b5e265f66bde28fb0b012ded1b00624c4df1c
Christopher Añorve <canorve@gmail.com>
2021-09-11 12:38:51 -0600

    On branch master 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+
+with open("requirements.txt", "r") as f:
+    reqs = [line.rstrip("\n") for line in f if line != "\n"]
+
+
+setup(
+
+    name="EllipSect", # Replace with your username
+
+    version="2.0.0",
+
+    author="Christopher Añorve, Emmanuel Ríos-López, Omar U. Reyes-Amador and Omar López-Cruz",
+
+    author_email="canorve@gmail.com",
+
+    description="An analysis tool for GALFIT output",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/canorve/EllipSect",
+
+    packages=find_packages(),
+
+    install_requires=reqs,
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License ::  GNU License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.8.5',
+
+)
+
+

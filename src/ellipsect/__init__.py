8b15c0a02a451fb1c663d17a4acdbf3efe307f00
Christopher Añorve <canorve@gmail.com>
2022-05-29 11:52:54 -0600

    bug that prevented to import functions in script was removed
     Changes to be committed:
    	modified:   src/ellipsect/__init__.py

--- src/ellipsect/__init__.py
+++ src/ellipsect/__init__.py
@@ -1,5 +1,8 @@
 import sys
 
+from ellipsect.inout.read import ArgParsing
+from ellipsect.sectors.sect import SectorsGalfit 
+
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
